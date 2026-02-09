7d26856a287354a0d1fb1b258ce9e181bc04fbec
ghsfrc5183 <frc5183@gmail.com>
2026-02-09 17:25:40 -0500

    feat(shooter): add a command to shoot

---
+++ src/main/kotlin/org/frc5183/robot/commands/shooter/ShootCommand.kt
@@ -0,0 +1,20 @@
+package org.frc5183.robot.commands.shooter
+
+import edu.wpi.first.wpilibj2.command.Command
+import org.frc5183.robot.subsystems.shooter.ShooterSubsystem
+
+class ShootCommand(val shooter: ShooterSubsystem) : Command() {
+    init {
+        addRequirements(shooter)
+    }
+
+    override fun initialize() {
+        shooter.run(1.0)
+    }
+
+    override fun end(interrupted: Boolean) {
+        shooter.stop()
+    }
+
+    override fun isFinished(): Boolean = false
+}
